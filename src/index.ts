6de23f033090fda605b8f6d5dca3636e976b7b45
Atabek <atabek122002@gmail.com>
2025-04-22 12:05:21 +0500

    типизировал код

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import {RequestWithBody, RequestWithParams, RequestWithParamsAndBody, RequestWithQuery} from "./types";
+import {CreateCourseModel} from "./models/CreateCourseModel";
+import {UpdateCourseModel} from "./models/UpdateCourseModel";
+import {QueryCoursesModel} from "./models/QueryCoursesModel";
+import {CourseViewModel} from "./models/CourseViewModel";
+import {URIParamsCourseIdModel} from "./models/URIParamsCourseIdModel";
 
 const app = express()
 const port = 3000
@@ -11,35 +17,48 @@ const HTTP_STATUSES = {
     BAD_REQUEST_400: 400,
     NOT_FOUND_404: 404,
 }
+const jsonBodyMiddleware = express.json()
+app.use(jsonBodyMiddleware)
 
-app.use(express.json())
+type CourseType = {
+    id: number
+    title: string
+    studentsCount: number
+}
 
-const db = {
+const db: { courses: CourseType[] } = {
     courses: [
-        { id: 1, title: "front-end" },
-        { id: 2, title: "back-end" },
-        { id: 3, title: "automation qa" },
-        { id: 4, title: "devops" },
+        { id: 1, title: "front-end", studentsCount: 10 },
+        { id: 2, title: "back-end", studentsCount: 10 },
+        { id: 3, title: "automation qa", studentsCount: 10 },
+        { id: 4, title: "devops", studentsCount: 10 },
     ]
 }
 
-app.get('/courses', (req, res) => {
+const getCourseViewModel = (dbCourse: CourseType): CourseViewModel => {
+    return {
+        id: dbCourse.id,
+        title: dbCourse.title,
+    }
+}
+
+app.get('/courses', (req: RequestWithQuery<QueryCoursesModel>, res: Response<CourseViewModel[]>) => {
     let foundCourses = db.courses;
     if(req.query.title) {
-        foundCourses = db.courses.filter(c => c.title.indexOf(req.query.title as string) > -1)
+        foundCourses = db.courses.filter(c => c.title.indexOf(req.query.title) > -1)
     }
 
-    res.json(foundCourses)
+    res.json(foundCourses.map(getCourseViewModel))
 })
-app.get('/courses/:id', (req, res) => {
+app.get('/courses/:id', (req: RequestWithParams<URIParamsCourseIdModel>, res: Response<CourseViewModel>) => {
     const foundCourse = db.courses.find((c) => c.id === +req.params.id)
     if(!foundCourse) {
         res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
         return;
     }
-    res.json(foundCourse)
+    res.json(getCourseViewModel(foundCourse))
 })
-app.post('/courses', (req, res) => {
+app.post('/courses', (req: RequestWithBody<CreateCourseModel>, res: Response<CourseViewModel>) => {
     if(!req.body.title) {
         res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
         return;
@@ -47,11 +66,12 @@ app.post('/courses', (req, res) => {
     const createdCourse = {
         id: +(new Date()),
         title: req.body.title,
+        studentsCount: 0,
     }
     db.courses.push(createdCourse)
-    res.status(HTTP_STATUSES.CREATED_201).json(createdCourse)
+    res.status(HTTP_STATUSES.CREATED_201).json(getCourseViewModel(createdCourse))
 })
-app.put('/courses/:id', (req, res) => {
+app.put('/courses/:id', (req: RequestWithParamsAndBody<URIParamsCourseIdModel, UpdateCourseModel>, res) => {
     if(!req.body.title) {
         res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
         return;
@@ -64,7 +84,7 @@ app.put('/courses/:id', (req, res) => {
     foundCourse.title = req.body.title;
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 })
-app.delete('/courses/:id', (req, res) => {
+app.delete('/courses/:id', (req: Request<URIParamsCourseIdModel>, res) => {
     if(db.courses.find((c) => c.id === +req.params.id)) {
     db.courses = db.courses.filter(c => c.id !== +req.params.id)
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
---
+++ src/models/QueryCoursesModel.ts
@@ -0,0 +1,6 @@
+export type QueryCoursesModel = {
+    /**
+     * This title should be included in the Title of for found Courses
+     */
+    title: string;
+}
